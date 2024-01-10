f3d61bbb2921b56c7b02b1ebc514357c5791eaef
Milos <milos.sstnaojevic@elfak.rs>
2024-01-10 10:42:20 +0100

    Changed gitIgnore

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+using static GlobalConstants;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private PoolingSystem _pool;
+    private const int OffsetZ = 4;
+    private const float edgePosX = 4f;
+    private const float posY = 4f;
+    private const float posZ = 35f;
+    private float obstacleSpawnDelay = 1f;
+    private float spaceshipSpawnDelay = 2f;
+    private bool canSpawn;
+
+    public void EnableSpawning() => canSpawn = true;
+    public void DisableSpawning() => canSpawn = false;
+    public float GetObstacleSpawnDelay() => obstacleSpawnDelay;
+    public float GetSpaceshipSpawnDelay() => spaceshipSpawnDelay;
+    public void SetObstacleSpawnDelay(float spawnDelayInSeconds) => obstacleSpawnDelay = spawnDelayInSeconds;
+    public void SetSpaceshipSpawnDelay(float spawnDelayInSeconds) => spaceshipSpawnDelay = spawnDelayInSeconds;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnObstacle());
+        StartCoroutine(SpawnSpaceship());
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(SpawnObstacle());
+        StopCoroutine(SpawnSpaceship());
+    }
+    public IEnumerator SpawnObstacle()
+    {
+        while (canSpawn)
+        {
+            yield return new WaitForSeconds(obstacleSpawnDelay);
+            HandleObstacleSpawning();
+        }
+    }
+
+    private void HandleObstacleSpawning()
+    {
+        ObstacleController obstacle = _pool.GetObstacleFromPool();
+
+        if (obstacle.name == "DoubleObstacleMiddle(Clone)")
+        {
+            obstacle.transform.position = new Vector3(obstacle.transform.position.x, obstacle.transform.position.y, posZ);
+        }
+        else
+        {
+            float randomXObst = Random.Range(-edgePosX, edgePosX);
+
+            obstacle.transform.position = new Vector3(randomXObst, obstacle.transform.position.y, posZ);
+        }
+    }
+
+    public IEnumerator SpawnSpaceship()
+    {
+        while (canSpawn)
+        {
+            yield return new WaitForSeconds(spaceshipSpawnDelay);
+            HandleSpaceshipSpawning();
+        }
+    }
+
+    private void HandleSpaceshipSpawning()
+    {
+        SpaceshipController spaceship = _pool.GetSpaceshipFromPool();
+
+        float randomXShip = Random.Range(-edgePosX, edgePosX);
+        float randomY = Random.Range(1f, posY);
+
+        spaceship.transform.position = new Vector3(randomXShip, randomY, posZ + OffsetZ);
+    }
+}
---
+++ Assets/Scripts/PoolingSystem.cs
@@ -0,0 +1,146 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using static GlobalConstants;
+
+public class PoolingSystem : MonoBehaviour
+{
+    private const int DefaultCapacityForPool = 10;
+    private const int MaximumCapacityForPool = 20;
+
+    [SerializeField] private Transform parentOfPool;
+    [SerializeField] private ObstacleController[] obstacle;
+    [SerializeField] private SpaceshipController spaceship;
+    private ObjectPool<ObstacleController> _poolObstacle;
+    private ObjectPool<SpaceshipController> _poolSpaceship;
+
+    private List<SpaceshipController> instantiatedSpaceships;
+    private List<ObstacleController> instantiatedObstacles;
+
+
+    private void Start()
+    {
+        instantiatedSpaceships = new List<SpaceshipController>();
+        instantiatedObstacles = new List<ObstacleController>();
+
+        instantiatedObstacles = CreateObstaclePool();
+        instantiatedSpaceships = CreateSpaceshipPool();
+    }
+
+    private void OnEnable()
+    {
+        SubscribeToDestroyAction();
+    }
+
+    private void SubscribeToDestroyAction()
+    {
+        EventManager.Instance.OnDestroyAction += DestroyObject;
+    }
+
+    private List<ObstacleController> CreateObstaclePool()
+    {
+        _poolObstacle = new ObjectPool<ObstacleController>(CreateObstacle, obstacle =>
+        {
+            obstacle.gameObject.SetActive(true);
+        }, obstacle =>
+        {
+            obstacle.gameObject.SetActive(false);
+        }, obstacle =>
+        {
+            UnsubscribeFromDestroyAction();
+            Destroy(obstacle.gameObject);
+        }, true, DefaultCapacityForPool, MaximumCapacityForPool);
+
+        return instantiatedObstacles;
+    }
+
+    private ObstacleController CreateObstacle()
+    {
+        int index = GenerateIndexWithProbability();
+        ObstacleController obs = Instantiate(obstacle[index]);
+        obs.transform.SetParent(parentOfPool);
+        instantiatedObstacles.Add(obs);
+
+        return obs;
+    }
+
+    private int GenerateIndexWithProbability()
+    {
+        int index = Random.Range(ZeroPercent, HundredPercent);
+        if (index <= ChanceForRoadblock)
+            return RoadblockIndex;
+        else
+            return DoubleConeIndex;
+    }
+
+    private List<SpaceshipController> CreateSpaceshipPool()
+    {
+        _poolSpaceship = new ObjectPool<SpaceshipController>(CreateSpaceship, spaceship =>
+        {
+            spaceship.gameObject.SetActive(true);
+        }, spaceship =>
+        {
+            spaceship.gameObject.SetActive(false);
+        }, spaceship =>
+        {
+            UnsubscribeFromDestroyAction();
+            Destroy(spaceship.gameObject);
+        }, true, DefaultCapacityForPool, MaximumCapacityForPool);
+
+        return instantiatedSpaceships;
+    }
+
+    private SpaceshipController CreateSpaceship()
+    {
+        SpaceshipController ship = Instantiate(spaceship);
+        ship.transform.SetParent(parentOfPool);
+        instantiatedSpaceships.Add(ship);
+        return ship;
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeFromDestroyAction();
+    }
+
+    private void UnsubscribeFromDestroyAction()
+    {
+        EventManager.Instance.OnDestroyAction -= DestroyObject;
+    }
+
+    public ObstacleController GetObstacleFromPool()
+    {
+        return _poolObstacle.Get();
+    }
+
+    public SpaceshipController GetSpaceshipFromPool()
+    {
+        return _poolSpaceship.Get();
+    }
+
+    private void DestroyObject(IDestroyable destroyable)
+    {
+        if (destroyable is ObstacleController obstacle)
+        {
+            _poolObstacle.Release(obstacle);
+        }
+        else if (destroyable is SpaceshipController spaceship)
+        {
+            _poolSpaceship.Release(spaceship);
+        }
+        else
+        {
+            Debug.LogError("Invalid destroyable type");
+        }
+    }
+
+    public List<SpaceshipController> GetInstanciatedSpaceships()
+    {
+        return instantiatedSpaceships;
+    }
+
+    public List<ObstacleController> GetInstanciatedObstacles()
+    {
+        return instantiatedObstacles;
+    }
+}
